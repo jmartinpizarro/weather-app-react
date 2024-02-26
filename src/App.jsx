0a30aeec0253cfb8834954a8eb2d8af0507ea833
Javier Martin <jmartinpizarro04@gmail.com>
2024-02-26 19:32:08 +0100

    Implement correct functionality of the fetch

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,37 @@
-import { useState } from 'react'
-import './App.css'
+import { useState, useEffect } from 'react';
+import './App.css';
 
-const API_KEY = `LEPAEYENMR9QWD2GPVE6Z535S`
-
+const API_KEY = `LEPAEYENMR9QWD2GPVE6Z535S`;
 
 function App() {
-
-  const [location, setLocation] = useState('')
+  const [location, setLocation] = useState('');
+  const [weatherData, setWeatherData] = useState(null); // for saving the weather changes
 
   const handleClick = (event) => {
-    event.preventDefault()
-    setLocation(document.getElementById('input').value)
-  }
-
-  useFetch(location)
-
-  function useFetch(location){ /* custom hook for fetching */
-    const BASE_URL = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=${API_KEY}`
+    event.preventDefault();
+    setLocation(document.getElementById('input').value);
+  };
+  
+  useEffect(() => {
+    if (location) {
+      useFetch(location)}
+  }, [location])
+  
+  function useFetch(location) { // custom hook for fetch
+    const BASE_URL = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=${API_KEY}`;
     fetch(BASE_URL)
-    .then(res => res.json())
-    .then(data => {
-      const {currentConditions} = data
-      console.log(currentConditions)
-    })
+      .then(res => res.json())
+      .then(data => {
+        const { currentConditions } = data;
+        setWeatherData(currentConditions);
+      })
+      .catch(error => {
+        console.error('Error fetching data:', error);
+      });
   }
 
   return (
-   <main>
+    <main>
       <header>
         <h1 className='main-h1'>weatherChecker = true</h1>
       </header>
@@ -35,8 +40,15 @@ function App() {
         <input type="text" placeholder='What city are you looking for?' className='input-form' id='input'/>
         <button className='input-btn' onClick={handleClick}>Show me the weather!</button>
       </form>
-   </main>
-  )
+
+      {/* if data is available */}
+      {weatherData && (
+        <div>
+          <h2>Weather for {location}</h2>
+        </div>
+      )}
+    </main>
+  );
 }
 
-export default App
+export default App;
